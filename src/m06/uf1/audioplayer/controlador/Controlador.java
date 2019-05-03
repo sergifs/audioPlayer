e0fe369261d3d5822ee64e03f94cd8d50b0e1658
Antonio <toni.cano.redondo@gmail.com>
2019-05-03 10:28:12 +0200

    Lista de canciones añadida y la ventana de Diseño de interfaz

--- src/m06/uf1/audioplayer/controlador/Controlador.java
+++ src/m06/uf1/audioplayer/controlador/Controlador.java
@@ -4,14 +4,18 @@ import m06.uf1.audioplayer.vista.Vista;
 import m06.uf1.audioplayer.modelo.Audio;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import javax.swing.JButton;
 import javazoom.jlgui.basicplayer.BasicPlayerException;
+import m06.uf1.audioplayer.vista.View;
 
-public class Controlador implements ActionListener {
+public class Controlador implements ActionListener, ItemListener {
 
     private Audio audio;
 
     public Controlador() throws BasicPlayerException {
-        Vista vista = new Vista(this);
+        new View(this).setVisible(true);
         audio = Audio.GetPlayer();
         audio.openSong(ReproductorAudio.buscarCancion(0));
     }
@@ -24,21 +28,27 @@ public class Controlador implements ActionListener {
 
         try {
             switch (action) {
-                case "play":
+                case "Play":
                     audio.play();
                     break;
-                case "stop":
+                case "Stop":
                     audio.stop();
                     break;
-                case "pausa":
+                case "Pause":
+                    JButton btn = (JButton) esdeveniment.getSource();
                     audio.pause();
                     break;
-                case "continuar":
+                case "Resume":
                     audio.resume();
                     break;
             }
         } catch (BasicPlayerException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent ie) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
---
+++ src/m06/uf1/audioplayer/modelo/TableModel.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m06.uf1.audioplayer.modelo;
+
+import java.util.Map;
+import javax.swing.table.AbstractTableModel;
+import m06.uf1.audioplayer.controlador.ReproductorAudio;
+/**
+ *
+ * @author Antonio
+ */
+public class TableModel extends AbstractTableModel{
+    
+    private Map<Integer, Cancion> canciones;
+    private String[] nombreColumna;
+    
+    public TableModel(String[] nombreColumna) {
+        canciones = ReproductorAudio.getCanciones();
+        this.nombreColumna = nombreColumna;
+    }
+    
+    @Override
+    public int getRowCount() {
+        return canciones.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 4;
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        Object obj = null;
+        Cancion c = canciones.get(i);
+        switch(i1){
+            case 0:
+                obj = c.getNombre();
+                break;
+            case 1:
+                obj = c.getAutor();
+                break;
+            case 2:
+                obj = c.getAlbum();
+                break;
+            case 3:
+                obj = c.getDuracion();
+                break;
+            default:
+                throw new UnsupportedOperationException();
+        }
+        return obj;
+    }
+
+    @Override
+    public String getColumnName(int i) {
+        return  nombreColumna[i];
+    }
+    
+}
