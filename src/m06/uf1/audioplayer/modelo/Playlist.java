ed8fa123a2fc1ddc23d5657a689a637a33c1c1c6
sergifs <sergi.f.sellares@gmail.com>
2019-05-03 13:36:51 +0200

    Metode de creacio del JSON modificat

--- src/m06/uf1/audioplayer/modelo/Playlist.java
+++ src/m06/uf1/audioplayer/modelo/Playlist.java
@@ -75,31 +75,31 @@ public class Playlist {
         JSONObject playList_01 = new JSONObject();
         playList_01.put("nombre", "Rap Playlist");
         playList_01.put("descripcion", "Playlist de rap");
-        playList_01.put("rutaImagen", "albumArt/Post Malone - Rockstar ft. 21 Savage.jpg");
+        playList_01.put("rutaImagen", "albumArt\\Post Malone - Rockstar ft. 21 Savage.jpg");
         JSONArray rutaArchivos_01 = new JSONArray();
-        rutaArchivos_01.add("audios/Post Malone - Rockstar ft. 21 Savage.mp3");
-        rutaArchivos_01.add("audios/Childish Gambino - This Is America.mp3");
-        rutaArchivos_01.add("audios/twenty one pilots Stressed Out.mp3");
+        rutaArchivos_01.add("audios\\Post Malone - Rockstar ft. 21 Savage.mp3");
+        rutaArchivos_01.add("audios\\Childish Gambino - This Is America.mp3");
+        rutaArchivos_01.add("audios\\twenty one pilots Stressed Out.mp3");
         playList_01.put("rutasArchivos", rutaArchivos_01);
 
         JSONObject playList_02 = new JSONObject();
         playList_02.put("nombre", "Happy Playlist");
         playList_02.put("descripcion", "Playlist para cuando estás de buen humor");
-        playList_02.put("rutaImagen", "albumArt/Sia - The Greatest.jpg");
+        playList_02.put("rutaImagen", "albumArt\\Sia - The Greatest.jpg");
         JSONArray rutaArchivos_02 = new JSONArray();
-        rutaArchivos_02.add("audios/Neovaii - Heart Shaped Box.mp3");
-        rutaArchivos_02.add("audios/Sia - The Greatest.mp3");
-        rutaArchivos_02.add("audios/David Guetta - Titanium ft. Sia.mp3");
+        rutaArchivos_02.add("audios\\Neovaii - Heart Shaped Box.mp3");
+        rutaArchivos_02.add("audios\\Sia - The Greatest.mp3");
+        rutaArchivos_02.add("audios\\David Guetta - Titanium ft. Sia.mp3");
         playList_02.put("rutasArchivos", rutaArchivos_02);
 
         JSONObject playList_03 = new JSONObject();
         playList_03.put("nombre", "Chill Playlist");
         playList_03.put("descripcion", "Playlist para estar del chill");
-        playList_03.put("rutaImagen", "albumArt/The Weeknd - Starboy ft. Daft Punk.jpg");
+        playList_03.put("rutaImagen", "albumArt\\The Weeknd - Starboy ft. Daft Punk.jpg");
         JSONArray rutaArchivos_03 = new JSONArray();
-        rutaArchivos_03.add("audios/The Weeknd - Starboy ft. Daft Punk.mp3");
-        rutaArchivos_03.add("audios/Stephen - Crossfire.mp3");
-        rutaArchivos_03.add("audios/Neovaii - Heart Shaped Box.mp3");
+        rutaArchivos_03.add("audios\\The Weeknd - Starboy ft. Daft Punk.mp3");
+        rutaArchivos_03.add("audios\\Stephen - Crossfire.mp3");
+        rutaArchivos_03.add("audios\\Neovaii - Heart Shaped Box.mp3");
         playList_03.put("rutasArchivos", rutaArchivos_03);
 
         try {
